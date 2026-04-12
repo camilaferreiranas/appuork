6c297df0a128f87c929f51e6e26e4a095015e496
Camila Ferreira <camila_fn97@hotmail.com>
2026-04-12 16:41:55 -0300

    feat: commit inicial

---
+++ src/main/java/br/com/uork/appuork/AppuorkApplication.java
@@ -0,0 +1,13 @@
+package br.com.uork.appuork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppuorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppuorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/uork/appuork/AppuorkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uork.appuork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppuorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
